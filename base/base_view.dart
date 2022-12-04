0ca26eac78d226f6c53e7aa4a45864f8833f2066
Mehmet175 <76875926+Mehmet175@users.noreply.github.com>
2022-12-05 01:21:07 +0300

    second_commit

--- base/base_view.dart
+++ base/base_view.dart
@@ -1,54 +1,70 @@
 import 'package:bionluk_otel/core/base/base_view_model.dart';
-import 'package:flutter/material.dart';
+import 'package:bionluk_otel/core/helper/mixin/state_mixin.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:get_it/get_it.dart';
 import 'package:provider/provider.dart';
 
 abstract class BaseView<T extends BaseViewModel> extends StatelessWidget {
   BaseView({super.key});
-  void onInit() {}
 
-  final viewModel = GetIt.instance<T>();
+  void initState() {}
+  void dispose() {}
+
+  late final T viewModel;
+  late final BuildContext context;
 
   @override
   Widget build(BuildContext context) {
-    return StatefulWrapper(
-      onInit: (){
-        onInit();
-        viewModel.onInit();
+    return BaseViewWrapper<T>(
+      initState: initState,
+      dispose: dispose,
+      onBuilder: (context, viewModel) {
+        this.viewModel = viewModel;
+        this.context = context;
+        return Consumer<T>(builder: (context, value, child) => onPageBuild());
       },
-      child: ChangeNotifierProvider(
-        create: (context) {
-          return viewModel;
-        },
-        child: onPageBuild(),
-      ),
     );
   }
 
   Widget onPageBuild();
 }
 
+class BaseViewWrapper<T extends BaseViewModel> extends StatefulWidget {
+  final VoidCallback? initState;
+  final VoidCallback? dispose;
+  final Widget Function(BuildContext context, T viewModel) onBuilder;
 
-class StatefulWrapper extends StatefulWidget {
-  final Function() onInit;
-  final Widget child;
-
-  const StatefulWrapper({required this.onInit, required this.child});
+  const BaseViewWrapper(
+      {super.key, required this.onBuilder, this.initState, this.dispose});
 
   @override
-  _StatefulWrapperState createState() => _StatefulWrapperState();
+  State<BaseViewWrapper<T>> createState() => _BaseViewWrapperState<T>();
 }
 
-class _StatefulWrapperState extends State<StatefulWrapper> {
+class _BaseViewWrapperState<T extends BaseViewModel>
+    extends State<BaseViewWrapper<T>> with StateMixinWait {
+  final _viewModel = GetIt.instance<T>();
 
   @override
   void initState() {
-    widget.onInit();
     super.initState();
+    waitForScreen(() {
+      _viewModel.onInit();
+    });
+    widget.initState?.call();
+  }
+
+  @override
+  void dispose() {
+    widget.dispose?.call();
+    super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
-    return widget.child;
+    return ChangeNotifierProvider(
+      create: (context) => _viewModel,
+      child: widget.onBuilder(context, _viewModel),
+    );
   }
 }
